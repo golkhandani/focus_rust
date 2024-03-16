ddda82b20a6df4c3997e5de1446efa6765132c17
golkhandani <site.golkhandani@gmail.com>
2024-03-15 23:58:20 -0700

    manual drop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug)]
+struct Rectangle {
+    x: f32,
+    y: f32,
+    title: String,
+}
+
+impl Drop for Rectangle {
+    fn drop(&mut self) {
+        println!("dropping rectangle {}", self.title)
+    }
+}
+
 fn main() {
+    {
+        let rect = Rectangle {
+            x: 2.32,
+            y: 23.0,
+            title: "Rectangle 1".to_string(),
+        };
+        println!("rect {:#?} = {} * {}", rect, rect.x, rect.y)
+    }
+
     println!("Focus Rust!")
 }
